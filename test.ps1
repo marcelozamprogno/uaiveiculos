f9b3ea8fb182160112e2fe37e746c25a34944ae0
Marcelo Zamprogno <marcelozamprogno@gmail.com>
2026-08-01 01:29:50 -0300

    Fix: resolve react state setter crash u('erro') -> c('erro')

--- test.ps1
+++ test.ps1
@@ -1,8 +1,10 @@
 $body = @{
-    name = "Marcio Silva"
-    cpf = "14924671622"
-    phone = "31999999999"
+    data = @{
+        cpf = "14924671622"
+        name = "Comprador"
+        phone = "31999999999"
+    }
 } | ConvertTo-Json
 
-$response = Invoke-RestMethod -Uri "https://uaiveiculos.vercel.app/api/create-pix" -Method POST -ContentType "application/json" -Body $body
+$response = Invoke-RestMethod -Uri "https://uaiveiculos.vercel.app/repasse/api/pix/create" -Method POST -ContentType "application/json" -Body $body
 $response | ConvertTo-Json -Depth 10
